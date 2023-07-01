4943f0df50bf9585ebc929bf6b899b9ad0c6579e
Jiwesh <dasjiwesh@gmail.com>
2023-07-01 08:09:33 +0530

    1st Commit

---
+++ alert.js
@@ -0,0 +1,81 @@
+// let age = prompt('How old r u ?');
+// alert(`U r ${age} years old`);
+
+// let isBoss = confirm('R U a boy ?');
+// alert(isBoss);
+
+// let value = true;
+// alert(typeof value);
+
+// value = String(value);
+// alert(typeof value);
+
+// let str = "123";
+// alert(typeof str);
+
+// str = Number(str);
+// alert(typeof str);
+
+// let num = Number("Hello");
+// alert(num);
+
+// let sum = (a, b) => a + b;
+// alert(sum(1,2));
+
+// let old = n => n * 2;
+// alert(old(5));
+
+// let sayHi = () => alert("Hello!");
+// sayHi();
+
+// let age = prompt("What's your age ?");
+// let welcome = (age > 18) ?
+//     () => alert('Adult') :
+//     () => alert('Minor');
+// welcome();
+
+// function ask(question, yes, no) {
+//     if (confirm(question))
+//     yes();
+//     else 
+//     no();
+//   }
+  
+//   ask(
+//     "Do you agree?",
+//     () => {alert("You agreed."); },
+//     () => {alert("You canceled the execution."); }
+//   );
+
+// let user = {
+//   name: 'Jiwesh',
+//   age: 25
+// }
+// alert(user.name);
+// alert(user.age);
+
+// user.gender = 'Male';
+// alert(user.gender);
+
+// let key = prompt('What do you want to display ?');
+// alert(user[key]);
+
+// let fruit = prompt('Which fruit you want to eat ?','');
+// let bag = {
+//   [fruit]: 5
+// }
+// alert(bag.apple);
+
+// function makeUser(name, age)
+// {
+//   return
+//   {
+//     name,
+//     age,
+//   };
+// }
+// let user = 
+// {
+//   name: 'John',
+//   age: 30
+// };
